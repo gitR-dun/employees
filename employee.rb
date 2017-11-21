4457d97d6a8a67dbd497db119f284ebefe81b8dd
Brian <riceb53@gmail.com>
2017-11-20 20:45:35 -0600

    Model employee data with hash

--- employee.rb
+++ employee.rb
@@ -13,3 +13,35 @@
 
 # p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year"
 
+# Hashes
+
+employee1 = {
+ "first_name" => "Majora",
+ "last_name" => "Carter",
+ "salary" => 80000,
+ "active" => true
+}
+
+
+# logical syntax
+employee2 = {
+  :first_name => "Danilo",
+  :last_name => "Campos",
+  :salary => 70000,
+  :active => true
+}
+
+# fancy syntax
+employee2 = {
+  first_name: "Danilo",
+  last_name: "Campos",
+  salary: 70000,
+  active: true
+}
+
+
+
+p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year"
+
+p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year"
+
