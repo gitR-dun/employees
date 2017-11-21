6a5eeb3cd7997650d90895d4fe218fbd59109c53
Brian <riceb53@gmail.com>
2017-11-20 21:31:12 -0600

    Make half of a class

--- employee.rb
+++ employee.rb
@@ -13,35 +13,48 @@
 
 # p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year"
 
-# Hashes
+# # Hashes
 
-employee1 = {
- "first_name" => "Majora",
- "last_name" => "Carter",
- "salary" => 80000,
- "active" => true
-}
-
-
-# logical syntax
-employee2 = {
-  :first_name => "Danilo",
-  :last_name => "Campos",
-  :salary => 70000,
-  :active => true
-}
-
-# fancy syntax
-employee2 = {
-  first_name: "Danilo",
-  last_name: "Campos",
-  salary: 70000,
-  active: true
-}
+# employee1 = {
+#  "first_name" => "Majora",
+#  "last_name" => "Carter",
+#  "salary" => 80000,
+#  "active" => true
+# }
 
 
 
-p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year"
+# # fancy syntax
+# employee2 = {
+#   first_name: "Danilo",
+#   last_name: "Campos",
+#   salary: 70000,
+#   active: true
+# }
 
-p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year"
+# # logical syntax
+# employee2 = {
+#   :first_name => "Danilo",
+#   :last_name => "Campos",
+#   :salary => 70000,
+#   :active => true
+# }
+
+# p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]} per year"
+
+
+# p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year"
+
+
+
+# classes
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+
+    p 'i am still running'
+  end
+end
+
+employee1 = Employee.new("Majora", "Carter", 80000, true)
 
