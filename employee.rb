9ac6e0bb3de762ec925b164a1895e647034bd428
Brian <riceb53@gmail.com>
2017-11-20 20:18:19 -0600

    Model employee data with array

---
+++ employee.rb
@@ -0,0 +1,15 @@
+# Arrays
+
+# # model some employees using ruby
+# # employee first name, last name, salary, active status
+
+# employee1 = ["Majora", "Carter", 80000, true]
+# employee2 = ["Danilo", "campos", 70000, false]
+
+# # print some information about the employees
+# # Majora Carter makes $80000 per year
+
+# p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
+
+# p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year"
+
