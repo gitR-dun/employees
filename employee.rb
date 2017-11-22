e79ebbb20b4c482cb8ede02d1e6b8d75d9040b87
Brian <riceb53@gmail.com>
2017-11-21 20:06:13 -0600

    add attr_reader and attr_writer

--- employee.rb
+++ employee.rb
@@ -45,37 +45,26 @@
 
 # p "#{employee1['first_name']} #{employee1['last_name']} makes $#{employee1['salary']} per year"
 
-
+# snake_case
+# first_name
+# camelCase
+# firstName
+# kebab-case
+# first-name
+# UpperCamelCase
+# FirstName
 
 # classes
 class Employee
+  attr_reader :first_name, :salary, :active
+  attr_accessor :last_name
+
   def initialize(input_first_name, input_last_name, input_salary, input_active)
     @first_name = input_first_name
     @last_name = input_last_name
     @salary = input_salary
     @active = input_active
   end
-
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  def active
-    @active
-  end
-
-  def last_name=(new_last_name)
-    @last_name = new_last_name
-  end
-
 
   def print_info
     "#{first_name} #{last_name} makes $#{salary} per year"
@@ -84,20 +73,18 @@ class Employee
   def give_annual_raise
     @salary = @salary * 1.05
   end
-
 end
-
-
 
 employee1 = Employee.new("Majora", "Carter", 80000, true)
 employee2 = Employee.new("Danilo", "Campos", 70000, false)
 
+# p employee1.last_name
 # employee1.last_name = "Campos"
-# p employee1
+# p employee1.last_name
 
 # p "before the raise"
-p employee1.print_info
-p employee2.print_info
+# p employee1.print_info
+# p employee2.print_info
 
 # employee1.give_annual_raise
 
